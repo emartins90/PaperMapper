e5517cf801c7a607e5fcfa9b863e32105063cce0
Emily Martins <emily.paige.martins@gmail.com>
2025-09-02 20:03:24 -0400

    dumb

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -36,10 +36,14 @@ function PostHogScript() {
           !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init Ce Os As Te Cs Fs capture Ye calculateEventProperties Ls register register_once register_for_session unregister unregister_for_session qs getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSurveysLoaded onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey canRenderSurveyAsync identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty zs js createPersonProfile Us Rs Bs opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing get_explicit_consent_status is_capturing clear_opt_in_out_capturing Ds debug L Ns getPageViewId captureTraceFeedback captureTraceMetric".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
           
           // Check consent BEFORE initializing PostHog
+          console.log('[DEBUG] Checking consent before PostHog init...');
           const consent = localStorage.getItem('cookie-consent');
           const hasConsent = consent === 'accepted';
+          console.log('[DEBUG] Consent status:', consent);
+          console.log('[DEBUG] Has consent:', hasConsent);
           
           // Initialize PostHog with the correct mode from the start
+          console.log('[DEBUG] Initializing PostHog with cookieless_mode:', hasConsent ? 'off' : 'always');
           posthog.init('${posthogKey}', {
             api_host: '${posthogHost}',
             defaults: '2025-05-24',
@@ -48,8 +52,18 @@ function PostHogScript() {
             advanced_enable_surveys: true, // Enable survey functionality
             // Only use cookieless mode if consent is NOT given
             cookieless_mode: hasConsent ? 'off' : 'always',
-            persistence: hasConsent ? 'localStorage+cookie' : 'memory'
+            persistence: hasConsent ? 'localStorage+cookie' : 'memory',
+            loaded: function(posthog) {
+              console.log('[DEBUG] PostHog loaded callback fired');
+              console.log('[DEBUG] PostHog config after init:', {
+                cookieless_mode: posthog.config.cookieless_mode,
+                advanced_enable_surveys: posthog.config.advanced_enable_surveys,
+                persistence: posthog.config.persistence
+              });
+            }
           });
+          
+          console.log('[DEBUG] PostHog init called');
           
           // No need to call opt_in/opt_out after initialization
           // PostHog is already initialized in the correct mode
