7835995408fa56ac677e10f5304d74ba1556dc8f
Emily Martins <emily.paige.martins@gmail.com>
2025-09-05 13:31:15 -0400

    update outline feature flag

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -3,9 +3,9 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from dotenv import load_dotenv
-#load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env.production'))
+load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env.production'))
 #load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env.development'))
-load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env.local'))
+#load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env.local'))
 
 
 from logging.config import fileConfig
